9e8a731d2ecce3165da1b87e8cf1693bb0504fb9
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-11 18:55:57 +0900

    update: タッチ操作および壁抜けの条件を修正

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -4,41 +4,62 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
-    // プレイヤーの移動の速さ
-    public float speed = 10f;
-    Rigidbody2D myRigidBody2d;
+  // プレイヤーの移動の速さ
+  public float speed = 10f;
+  Rigidbody2D myRigidBody2d;
 
-    // Start is called before the first frame update
-    void Start()
+  private float deltaX, deltaY;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    myRigidBody2d = GetComponent<Rigidbody2D>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (Input.GetKey(KeyCode.RightArrow))
     {
-        myRigidBody2d = GetComponent<Rigidbody2D>();
+      if (transform.position.x < 1.8)
+      {
+        transform.position += Vector3.right * Time.deltaTime * speed;
+      }
+    }
+    else if (Input.GetKey(KeyCode.LeftArrow))
+    {
+      if (transform.position.x > -1.8)
+      {
+        transform.position += Vector3.left * Time.deltaTime * speed;
+      }
     }
 
-    // Update is called once per frame
-    void Update()
+    // プレイヤーの移動(スマホでのタッチ操作)
+    if (Input.touchCount > 0)
     {
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if(transform.position.x < 1.8){
-                transform.position += Vector3.right * Time.deltaTime * speed;
-            }
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if(transform.position.x > -1.8){
-                transform.position += Vector3.left * Time.deltaTime * speed;
-            }
-        }
+      // タッチ情報の取得
+      Touch touch = Input.GetTouch(0);
 
-        // プレイヤーの移動(スマホでのタッチ操作)
-        if(Input.touchCount == 1)
-        {
-            // タッチ情報の取得
-            Touch touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Moved)
-            {
-                transform.position += new Vector3(touch.position.x, 0, 0);
-            }     
-        }
+      Vector2 touchPos = Camera.main.ScreenToWorldPoint(touch.position);
+
+      switch (touch.phase)
+      {
+        case TouchPhase.Began:
+          Debug.Log(touch.phase);
+          Debug.Log(touchPos);
+          deltaX = touchPos.x - transform.position.x;
+          deltaY = touchPos.y - transform.position.y;
+          break;
+
+        case TouchPhase.Moved:
+          myRigidBody2d.MovePosition(new Vector2(touchPos.x - deltaX, touchPos.y - deltaY));
+          Debug.Log(this.transform.position);
+          break;
+
+        case TouchPhase.Ended:
+          myRigidBody2d.velocity = Vector2.zero;
+          break;
+      }
     }
+  }
 }
