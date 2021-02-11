f937aac6b83bf46e747029ae6dd3f95102fad4d6
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-12 00:17:45 +0900

    update: 敵のバーの移動処理を追加

--- Assets/Scripts/EnemyBarManager.cs
+++ Assets/Scripts/EnemyBarManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class EnemyBarManager : MonoBehaviour
 {
+  // 敵のバーのスピード
+  public float speed = 3f;
+  GameObject ball;
+  Rigidbody2D ballrb;
+
   // Start is called before the first frame update
   void Start()
   {
-
+    ball = GameObject.Find("Ball");
+    ballrb = ball.GetComponent<Rigidbody2D>();
   }
 
   // Update is called once per frame
   void Update()
   {
-
+    {
+      // ボールが右向きに動いている時
+      if (ballrb.velocity.x > 0)
+      {
+        // 敵のバーも右向きに動く
+        if (this.transform.position.x < 1.8)
+        {
+          this.transform.position += Vector3.right * Time.deltaTime * speed;
+        }
+      }
+      // ボールが左向きに動いている時
+      else if (ballrb.velocity.x < 0)
+      {
+        if (this.transform.position.x > -1.8)
+        {
+          this.transform.position += Vector3.left * Time.deltaTime * speed;
+        }
+      }
+    }
   }
 }
