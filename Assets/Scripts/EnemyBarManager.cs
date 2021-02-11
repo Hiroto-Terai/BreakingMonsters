55c3d7571a68e1ab9f9903260ef7791ef04a9048
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-11 21:46:56 +0900

    add: 敵のバーのスクリプトを追加

---
+++ Assets/Scripts/EnemyBarManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBarManager : MonoBehaviour
+{
+  GameObject ball;
+  transform
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    ball = GameObject.Find("ball");
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+}
