06e641a510e5ba86c1d176065280a28ba94fcb3f
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-12 13:58:40 +0900

    update: ゲームオーバー処理を実装

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameOver : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  public Text gameOverText;
+  // スライムを持つ親オブジェクトを取得
+  Transform myTransform;
+  // ゲームオーバーの判定
+  bool isGameOver = false;
+  // Start is called before the first frame update
+  void Start()
+  {
+    myTransform = this.transform;
+  }
 
-    // Update is called once per frame
-    void Update()
+  // Update is called once per frame
+  void Update()
+  {
+    if (myTransform.childCount == 0)
     {
-        
+      gameOverText.text = "Game Over...";
+      Time.timeScale = 0f;
+      isGameOver = true;
     }
+  }
 }
