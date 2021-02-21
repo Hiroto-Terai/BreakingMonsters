cea9b806b6d215976d0a235ee2136687df41af35
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-21 16:13:46 +0900

    update: ゲームオーバSE実装

--- Assets/Scripts/GameClear.cs
+++ Assets/Scripts/GameClear.cs
@@ -9,11 +9,21 @@ public class GameClear : MonoBehaviour
   public Text gameClearText;
   Transform myTransform;
   bool isGameClear = false;
+  // BGMを持っているオブジェクト
+  public GameObject Music;
+  private Transform MusicTransform;
+  // ゲームクリアサウンド
+  // update関数内で一回だけ呼び出すためのフラグを用意
+  public AudioClip GameClearSound;
+  bool isCalledOnce;
 
   // Start is called before the first frame update
   void Start()
   {
     myTransform = transform;
+    MusicTransform = Music.GetComponent<Transform>();
+    // update関数内で一回だけ呼び出すためのフラグを初期化
+    isCalledOnce = false;
   }
 
   // Update is called once per frame
@@ -22,6 +32,14 @@ public class GameClear : MonoBehaviour
     if (myTransform.childCount == 0)
     {
       gameClearText.text = "Game Clear!!";
+      // BGMの停止
+      MusicTransform.GetChild(0).gameObject.SetActive(false);
+      // ゲームクリアのSEを鳴らす
+      if (!isCalledOnce)
+      {
+        isCalledOnce = true;
+        AudioSource.PlayClipAtPoint(GameClearSound, transform.position);
+      }
       Time.timeScale = 0f;
       isGameClear = true;
     }
