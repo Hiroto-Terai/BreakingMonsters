3c10a5a905fa17c2891d0fa47ba3f152e8f02a65
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-23 02:35:58 +0900

    update: オプションメニューの開閉処理を追加

--- Assets/Scripts/OptionManager.cs
+++ Assets/Scripts/OptionManager.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionManager : MonoBehaviour
 {
+  // オプションキャンバスのゲームオブジェクト
+  public GameObject OptionCanvas;
+
   // Start is called before the first frame update
   void Start()
   {
-
+    OptionCanvas.SetActive(false);
   }
 
   // Update is called once per frame
@@ -18,6 +22,11 @@ public class OptionManager : MonoBehaviour
 
   public void openOption()
   {
-    Debug.Log("オプションが開かれた");
+    OptionCanvas.SetActive(true);
+  }
+
+  public void closeOption()
+  {
+    OptionCanvas.SetActive(false);
   }
 }
