3d62e9213001f96131e8263457abe55f9990aa23
Hiroto-Terai <hiro08051610@gmail.com>
2021-02-23 11:28:57 +0900

    update: BGMのオンオフ設定をシーン間で共有できるよう実装

--- Assets/Scripts/OptionManager.cs
+++ Assets/Scripts/OptionManager.cs
@@ -2,16 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class OptionManager : MonoBehaviour
 {
   // オプションキャンバスのゲームオブジェクト
   public GameObject OptionCanvas;
+  private string TwitterURL;
+  private string InstagramURL;
+  private string PrivacyPolicyURL;
+
+  // BGMが再生されているか
+  public static bool isBgmPlaying = true;
+
+  // SEが再生されているか
+  public static bool isSePlaying;
+
+  public GameObject Music;
 
   // Start is called before the first frame update
   void Start()
   {
-    OptionCanvas.SetActive(false);
+    TwitterURL = "https://twitter.com/UVER_Hroxas";
+    InstagramURL = "https://www.instagram.com/?hl=ja";
+    PrivacyPolicyURL = "https://hiroto-terai.github.io/BreakingMonsters/";
+    Music = GameObject.Find("Music");
+
+    if (OptionManager.isBgmPlaying == true)
+    {
+      Music.GetComponent<AudioSource>().volume = 0.15f;
+      this.gameObject.transform.GetChild(1).gameObject.SetActive(false);
+    }
+    else
+    {
+      Music.GetComponent<AudioSource>().volume = 0f;
+      this.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+    }
   }
 
   // Update is called once per frame
@@ -23,10 +50,51 @@ public class OptionManager : MonoBehaviour
   public void openOption()
   {
     OptionCanvas.SetActive(true);
+    Time.timeScale = 0f;
   }
 
   public void closeOption()
   {
     OptionCanvas.SetActive(false);
+    Time.timeScale = 1f;
+  }
+
+  // Twitterを開く
+  public void openTwitter()
+  {
+    Application.OpenURL(TwitterURL);
+  }
+
+  //インスタを開く
+  public void openInstagram()
+  {
+    Application.OpenURL(InstagramURL);
+  }
+
+  public void openPrivacyPolicy()
+  {
+    Application.OpenURL(PrivacyPolicyURL);
+  }
+
+  public void goToMenuScene()
+  {
+    SceneManager.LoadScene("Menu", LoadSceneMode.Single);
+  }
+
+  public void BgmController()
+  {
+    if (isBgmPlaying == true)
+    {
+      isBgmPlaying = false;
+      Music.GetComponent<AudioSource>().volume = 0f;
+      this.gameObject.transform.GetChild(1).gameObject.SetActive(true);
+    }
+    else
+    {
+      isBgmPlaying = true;
+      Music.GetComponent<AudioSource>().volume = 0.15f;
+      this.gameObject.transform.GetChild(1).gameObject.SetActive(false);
+    }
+
   }
 }
---
+++ Assets/Scripts/getBgmStatus.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class getBgmStatus : MonoBehaviour
+{
+  // Start is called before the first frame update
+  void Start()
+  {
+    if (SceneManager.GetActiveScene().name == "Menu")
+    {
+      getBgmStatusInMenuScene();
+    }
+    else if (SceneManager.GetActiveScene().name == "EasyPlayMenu")
+    {
+      getBgmStatusInPlayScene();
+    }
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public bool getBgmStatusInMenuScene()
+  {
+    return OptionManager.isBgmPlaying;
+  }
+
+  public bool getBgmStatusInPlayScene()
+  {
+    return OptionManager.isBgmPlaying;
+  }
+}
