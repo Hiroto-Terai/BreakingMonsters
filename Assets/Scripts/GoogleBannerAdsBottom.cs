765c1c6b4d5087e1567210b88c07b0fc54de02e4
Hiroto-Terai <hiro08051610@gmail.com>
2021-03-03 01:00:34 +0900

    update: オブジェクトの配置を画面サイズに合わせて動的に設定、バナー広告を実装

---
+++ Assets/Scripts/GoogleBannerAdsBottom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds.Api;
+
+public class GoogleBannerAdsTop : MonoBehaviour
+{
+  private BannerView bannerView;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    // Google Mobile Ads SDKの初期化
+    MobileAds.Initialize(initStatus => { });
+
+    this.RequestBanner();
+  }
+  private void RequestBanner()
+  {
+    string adUnitId = "ca-app-pub-3940256099942544/6300978111";
+
+    // Create a 320x50 banner at the top of the screen.
+    this.bannerView = new BannerView(adUnitId, AdSize.Banner, AdPosition.Top);
+
+    // Create an empty ad request.
+    AdRequest request = new AdRequest.Builder().Build();
+
+    // Load the banner with the request.
+    this.bannerView.LoadAd(request);
+  }
+}
